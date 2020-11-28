d6635f039c0ec8090939416fcb8838110771f4fe
Gleb <neurothrop@gmail.com>
2020-11-28 12:11:54 +0300

    Update interface; Move code to new paths;
    Signed-off-by: Gleb <neurothrop@gmail.com>

---
+++ Assets/_iMyself/HypercasualCore/Code/IGameManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IGameManager
+{
+	event Action winLevel;
+	event Action looseLevel;
+
+	void LoadLevel(int index);
+	
+	void ResetLevel();
+	
+	void StartLevel();
+	
+	void PauseLevel(bool pause);
+}
